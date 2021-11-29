f249a501d3e1ae005b764d2713b0274f49f032f6
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-11-29 16:51:28 +0100

    Update friend.ts
    tried to do a function to access the input value, but it is not correct. It is only the begin

--- angular/project-name/src/app/friend.ts
+++ angular/project-name/src/app/friend.ts
@@ -1,2 +1,12 @@
 export class Friend {
+
+    function friend (firstName:string, lastName:string, email:string, phone:number, language:string): void {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.language = language;
+        
+    }
 }
