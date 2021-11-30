aeed527865cf5ab58cf5b13ea7aa53d31073fa92
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-11-30 15:12:45 +0100

    Update app.component.ts
    imported my service;
    Added a private "addFriendService" property;
    called the addFriend method of the addFriendService and passed the friend data;

--- angular/project-name/src/app/app.component.ts
+++ angular/project-name/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Friend } from './friend';
+import {AddFriendService} from "./add-friend.service";
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,10 @@ export class AppComponent {
   friend = new Friend("","","","","");
 
   getFriend(){
-    console.log(this.friend);
+    this.addFriendService.addFriend(this.friend);
+  }
+
+  constructor(private addFriendService: AddFriendService){
   }
 }
 
