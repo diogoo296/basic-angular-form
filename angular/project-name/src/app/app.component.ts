4843143832a334a3e6e254c4548fecbecf4eafef
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-01 11:15:19 +0100

    Update app.component.ts
    Made a async function to access the friendapi's data.
    
     the step 17 is almost done. I have to make the function to print the in the page after I add a new friend

--- angular/project-name/src/app/app.component.ts
+++ angular/project-name/src/app/app.component.ts
@@ -12,10 +12,23 @@ export class AppComponent {
   friend = new Friend("","","","","");
 
   getFriend(){
-    this.addFriendService.addFriend(this.friend).subscribe(data => "it worked", error => "it didn't work");
+    this.addFriendService.addFriend(this.friend).subscribe(data => console.log("it worked"), error => console.log("it didn't work"));
   }
 
   constructor(private addFriendService: AddFriendService){
+  }
+
+  ngOnInit(): void {
+    this.showFriends();
+  }
+
+  allFriends:any
+  async showFriends(): Promise<void> {
+    let apiFriend = await fetch ("http://localhost:9000/allFriends", {headers: {
+      'Content-Type': 'application/json'}});
+      this.allFriends = await apiFriend.json();
+
+    console.log(this.allFriends[1].firstName);
   }
 }
 
